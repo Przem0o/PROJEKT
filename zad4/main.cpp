76774ee50a61ef18b7a606e9b55262f5c0dd8e2e
Przem0o <przemek.litwa@interia.pl>
2022-01-18 00:46:56 +0100

    Add node and add lista

--- zad4/main.cpp
+++ zad4/main.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
 
 using namespace std;
+struct node
+{
+    int liczba;
+    node *prew;
+    node *next;
+};
 
+class lista
+{
+public:
+    node *begin;
+    node *end;
+    unsigned int count;
+
+    lista();
+    ~lista();
+
+    void push_front( int licz);
+    void push_back( int licz);
+    void pop_front();
+    void pop_back();
+    void
+
+};
 int main()
 {
-    cout << "Hello world!" << endl;
     return 0;
 }
